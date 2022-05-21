e3f1d6c285f9409598189cebdbbd206e56af5699
noe <noe@marleix.com>
2022-05-22 01:37:37 +0200

    ok

--- src/main/java/com/example/tutoforreal/Controller.java
+++ src/main/java/com/example/tutoforreal/Controller.java
@@ -2,12 +2,10 @@ package com.example.tutoforreal;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.xml.transform.Result;
@@ -16,45 +14,76 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
     private TableView<Project> projectTableView;
 
-
     @FXML
     private Label pageLabel;
 
+    @FXML
+    private TextField newProjectTitle;
+
+    @FXML
+    private TextArea newProjectDescription;
+
+    @FXML
+    private TableColumn<Project, String> projetColumnId;
+
+    @FXML
+    private TableColumn<Project, String> projetColumnTitle;
+
+    @FXML
+    private TableColumn<Project, String> projetColumnDescription;
+
+    ObservableList<Project>projectList = FXCollections.observableArrayList();
 
     @Override
     public void initialize(URL location, ResourceBundle ressources) {
-        TableColumn id = new TableColumn("ID");
-        TableColumn title = new TableColumn("TITLE");
-        TableColumn description = new TableColumn("DESCRIPTION");
-        projectTableView.getColumns().addAll(id, title, description);
-        ObservableList<Project>projectList = FXCollections.observableArrayList();
         DatabaseConnection connectNow = new DatabaseConnection();
         Connection connectDB = connectNow.getDbConnection();
-        String connectQuerry = "SELECT * FROM project";
+        ProjectQueries projectQueries = new ProjectQueries(connectNow);
+        getAllProjectsController(projectQueries);
+    }
 
-        try {
-            Statement statement = connectDB.createStatement();
-            ResultSet queryOutput = statement.executeQuery(connectQuerry);
-            while (queryOutput.next()) {
-                int idP = queryOutput.getInt("id");
-                String titleP = queryOutput.getString("title");
-                String descriptionP = queryOutput.getString("description");
-                Project newProject = new Project(idP, titleP, descriptionP);
-                projectList.add(newProject);
-                pageLabel.setText("Liste de vos projets : ");
+    public void getAllProjectsController (ProjectQueries projectQueries){
+            ObservableList<Project>projectList = FXCollections.observableArrayList();
+            pageLabel.setText("Liste de vos projets : ");
+            try {
+                ArrayList<Project>queryOutput = projectQueries.getAllProject();
+                int size = queryOutput.size();
+                for (int cpt = 0; cpt < size; cpt++) {
+                    Project p = queryOutput.get(cpt);
+                    projectList.add(p);
+
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
-        } catch (SQLException e) {
-            e.printStackTrace();
+            projetColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+            projetColumnTitle.setCellValueFactory(new PropertyValueFactory<>("title"));
+            projetColumnDescription.setCellValueFactory(new PropertyValueFactory<>("description"));
+
+            projectTableView.setItems(projectList);
+    }
+
+    public void createProject(){
+        String title = newProjectTitle.getText();
+        String description = newProjectDescription.getText();
+        Project p = new Project(title, description);
+        DatabaseConnection connectNow = new DatabaseConnection();
+        Connection connectDB = connectNow.getDbConnection();
+        ProjectQueries projectQueries = new ProjectQueries(connectNow);
+        try {
+            projectQueries.createProject(p);
+            projectTableView.getColumns().removeAll();
+            getAllProjectsController(projectQueries);
         }
-        id.setCellValueFactory(new PropertyValueFactory<Project, String>("id"));
-        title.setCellValueFactory(new PropertyValueFactory<Project, String>("title"));
-        description.setCellValueFactory(new PropertyValueFactory<Project, String>("description"));
-        projectTableView.setItems(projectList);
+        catch (SQLException e){
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/com/example/tutoforreal/HelloApplication.java
+++ src/main/java/com/example/tutoforreal/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("project.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
         //stage.setTitle("SQLITE connection");
         stage.initStyle(StageStyle.UTILITY);
         stage.setScene(scene);
